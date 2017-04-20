a16a4310ee2e2ceb0a69f7c54ac8dc06559923d7
Andrii Krupka <akrupka@deloitte.com>
2017-04-20 18:19:38 +0300

    add swipe refresh layout

--- UrbanDictionary.Xamarin/UrbanDictionary.Xamarin.Droid/Resources/Resource.designer.cs
+++ UrbanDictionary.Xamarin/UrbanDictionary.Xamarin.Droid/Resources/Resource.designer.cs
@@ -3899,26 +3899,8 @@ namespace UrbanDictionary.Xamarin.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f02006a
-			public const int avd_hide_password_1 = 2130837610;
-			
-			// aapt resource value: 0x7f02006b
-			public const int avd_hide_password_2 = 2130837611;
-			
-			// aapt resource value: 0x7f02006c
-			public const int avd_hide_password_3 = 2130837612;
-			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
-			
-			// aapt resource value: 0x7f02006d
-			public const int avd_show_password_1 = 2130837613;
-			
-			// aapt resource value: 0x7f02006e
-			public const int avd_show_password_2 = 2130837614;
-			
-			// aapt resource value: 0x7f02006f
-			public const int avd_show_password_3 = 2130837615;
 			
 			// aapt resource value: 0x7f020055
 			public const int design_bottom_navigation_item_background = 2130837589;
@@ -4002,8 +3984,8 @@ namespace UrbanDictionary.Xamarin.Droid
 			// aapt resource value: 0x7f070010
 			public const int MvxBindingTagUnique = 2131165200;
 			
-			// aapt resource value: 0x7f070087
-			public const int action0 = 2131165319;
+			// aapt resource value: 0x7f070088
+			public const int action0 = 2131165320;
 			
 			// aapt resource value: 0x7f070066
 			public const int action_bar = 2131165286;
@@ -4026,17 +4008,17 @@ namespace UrbanDictionary.Xamarin.Droid
 			// aapt resource value: 0x7f070043
 			public const int action_bar_title = 2131165251;
 			
-			// aapt resource value: 0x7f070084
-			public const int action_container = 2131165316;
+			// aapt resource value: 0x7f070085
+			public const int action_container = 2131165317;
 			
 			// aapt resource value: 0x7f070067
 			public const int action_context_bar = 2131165287;
 			
-			// aapt resource value: 0x7f07008b
-			public const int action_divider = 2131165323;
+			// aapt resource value: 0x7f07008c
+			public const int action_divider = 2131165324;
 			
-			// aapt resource value: 0x7f070085
-			public const int action_image = 2131165317;
+			// aapt resource value: 0x7f070086
+			public const int action_image = 2131165318;
 			
 			// aapt resource value: 0x7f070003
 			public const int action_menu_divider = 2131165187;
@@ -4053,20 +4035,20 @@ namespace UrbanDictionary.Xamarin.Droid
 			// aapt resource value: 0x7f070045
 			public const int action_mode_close_button = 2131165253;
 			
-			// aapt resource value: 0x7f07009d
-			public const int action_search = 2131165341;
+			// aapt resource value: 0x7f07009e
+			public const int action_search = 2131165342;
 			
-			// aapt resource value: 0x7f070086
-			public const int action_text = 2131165318;
+			// aapt resource value: 0x7f070087
+			public const int action_text = 2131165319;
 			
-			// aapt resource value: 0x7f070094
-			public const int actions = 2131165332;
+			// aapt resource value: 0x7f070095
+			public const int actions = 2131165333;
 			
 			// aapt resource value: 0x7f070046
 			public const int activity_chooser_view_content = 2131165254;
 			
-			// aapt resource value: 0x7f070083
-			public const int actualtabcontent = 2131165315;
+			// aapt resource value: 0x7f070084
+			public const int actualtabcontent = 2131165316;
 			
 			// aapt resource value: 0x7f07001b
 			public const int add = 2131165211;
@@ -4092,8 +4074,8 @@ namespace UrbanDictionary.Xamarin.Droid
 			// aapt resource value: 0x7f07004d
 			public const int buttonPanel = 2131165261;
 			
-			// aapt resource value: 0x7f070088
-			public const int cancel_action = 2131165320;
+			// aapt resource value: 0x7f070089
+			public const int cancel_action = 2131165321;
 			
 			// aapt resource value: 0x7f070032
 			public const int center = 2131165234;
@@ -4107,8 +4089,8 @@ namespace UrbanDictionary.Xamarin.Droid
 			// aapt resource value: 0x7f07005d
 			public const int checkbox = 2131165277;
 			
-			// aapt resource value: 0x7f070090
-			public const int chronometer = 2131165328;
+			// aapt resource value: 0x7f070091
+			public const int chronometer = 2131165329;
 			
 			// aapt resource value: 0x7f07003b
 			public const int clip_horizontal = 2131165243;
@@ -4134,20 +4116,20 @@ namespace UrbanDictionary.Xamarin.Droid
 			// aapt resource value: 0x7f070049
 			public const int default_activity_button = 2131165257;
 			
-			// aapt resource value: 0x7f070079
-			public const int design_bottom_sheet = 2131165305;
+			// aapt resource value: 0x7f07007a
+			public const int design_bottom_sheet = 2131165306;
+			
+			// aapt resource value: 0x7f070081
+			public const int design_menu_item_action_area = 2131165313;
 			
 			// aapt resource value: 0x7f070080
-			public const int design_menu_item_action_area = 2131165312;
+			public const int design_menu_item_action_area_stub = 2131165312;
 			
 			// aapt resource value: 0x7f07007f
-			public const int design_menu_item_action_area_stub = 2131165311;
+			public const int design_menu_item_text = 2131165311;
 			
 			// aapt resource value: 0x7f07007e
-			public const int design_menu_item_text = 2131165310;
-			
-			// aapt resource value: 0x7f07007d
-			public const int design_navigation_view = 2131165309;
+			public const int design_navigation_view = 2131165310;
 			
 			// aapt resource value: 0x7f070014
 			public const int disableHome = 2131165204;
@@ -4158,8 +4140,8 @@ namespace UrbanDictionary.Xamarin.Droid
 			// aapt resource value: 0x7f070023
 			public const int end = 2131165219;
 			
-			// aapt resource value: 0x7f07009a
-			public const int end_padder = 2131165338;
+			// aapt resource value: 0x7f07009b
+			public const int end_padder = 2131165339;
 			
 			// aapt resource value: 0x7f07002c
 			public const int enterAlways = 2131165228;
@@ -4197,8 +4179,8 @@ namespace UrbanDictionary.Xamarin.Droid
 			// aapt resource value: 0x7f07004b
 			public const int icon = 2131165259;
 			
-			// aapt resource value: 0x7f070095
-			public const int icon_group = 2131165333;
+			// aapt resource value: 0x7f070096
+			public const int icon_group = 2131165334;
 			
 			// aapt resource value: 0x7f070027
 			public const int ifRoom = 2131165223;
@@ -4206,23 +4188,23 @@ namespace UrbanDictionary.Xamarin.Droid
 			// aapt resource value: 0x7f070048
 			public const int image = 2131165256;
 			
-			// aapt resource value: 0x7f070091
-			public const int info = 2131165329;
+			// aapt resource value: 0x7f070092
+			public const int info = 2131165330;
 			
 			// aapt resource value: 0x7f070000
 			public const int item_touch_helper_previous_elevation = 2131165184;
 			
-			// aapt resource value: 0x7f070077
-			public const int largeLabel = 2131165303;
+			// aapt resource value: 0x7f070078
+			public const int largeLabel = 2131165304;
 			
 			// aapt resource value: 0x7f070036
 			public const int left = 2131165238;
 			
-			// aapt resource value: 0x7f070096
-			public const int line1 = 2131165334;
+			// aapt resource value: 0x7f070097
+			public const int line1 = 2131165335;
 			
-			// aapt resource value: 0x7f070098
-			public const int line3 = 2131165336;
+			// aapt resource value: 0x7f070099
+			public const int line3 = 2131165337;
 			
 			// aapt resource value: 0x7f070011
 			public const int listMode = 2131165201;
@@ -4230,11 +4212,11 @@ namespace UrbanDictionary.Xamarin.Droid
 			// aapt resource value: 0x7f07004a
 			public const int list_item = 2131165258;
 			
-			// aapt resource value: 0x7f07009c
-			public const int masked = 2131165340;
+			// aapt resource value: 0x7f07009d
+			public const int masked = 2131165341;
 			
-			// aapt resource value: 0x7f07008a
-			public const int media_actions = 2131165322;
+			// aapt resource value: 0x7f07008b
+			public const int media_actions = 2131165323;
 			
 			// aapt resource value: 0x7f070024
 			public const int middle = 2131165220;
@@ -4245,8 +4227,8 @@ namespace UrbanDictionary.Xamarin.Droid
 			// aapt resource value: 0x7f07001c
 			public const int multiply = 2131165212;
 			
-			// aapt resource value: 0x7f07007c
-			public const int navigation_header_container = 2131165308;
+			// aapt resource value: 0x7f07007d
+			public const int navigation_header_container = 2131165309;
 			
 			// aapt resource value: 0x7f070028
 			public const int never = 2131165224;
@@ -4257,14 +4239,14 @@ namespace UrbanDictionary.Xamarin.Droid
 			// aapt resource value: 0x7f070012
 			public const int normal = 2131165202;
 			
-			// aapt resource value: 0x7f070093
-			public const int notification_background = 2131165331;
+			// aapt resource value: 0x7f070094
+			public const int notification_background = 2131165332;
+			
+			// aapt resource value: 0x7f07008e
+			public const int notification_main_column = 2131165326;
 			
 			// aapt resource value: 0x7f07008d
-			public const int notification_main_column = 2131165325;
-			
-			// aapt resource value: 0x7f07008c
-			public const int notification_main_column_container = 2131165324;
+			public const int notification_main_column_container = 2131165325;
 			
 			// aapt resource value: 0x7f070039
 			public const int parallax = 2131165241;
@@ -4287,11 +4269,11 @@ namespace UrbanDictionary.Xamarin.Droid
 			// aapt resource value: 0x7f070037
 			public const int right = 2131165239;
 			
-			// aapt resource value: 0x7f070092
-			public const int right_icon = 2131165330;
+			// aapt resource value: 0x7f070093
+			public const int right_icon = 2131165331;
 			
-			// aapt resource value: 0x7f07008e
-			public const int right_side = 2131165326;
+			// aapt resource value: 0x7f07008f
+			public const int right_side = 2131165327;
 			
 			// aapt resource value: 0x7f07001d
 			public const int screen = 2131165213;
@@ -4356,14 +4338,14 @@ namespace UrbanDictionary.Xamarin.Droid
 			// aapt resource value: 0x7f070019
 			public const int showTitle = 2131165209;
 			
-			// aapt resource value: 0x7f070076
-			public const int smallLabel = 2131165302;
+			// aapt resource value: 0x7f070077
+			public const int smallLabel = 2131165303;
+			
+			// aapt resource value: 0x7f07007c
+			public const int snackbar_action = 2131165308;
 			
 			// aapt resource value: 0x7f07007b
-			public const int snackbar_action = 2131165307;
-			
-			// aapt resource value: 0x7f07007a
-			public const int snackbar_text = 2131165306;
+			public const int snackbar_text = 2131165307;
 			
 			// aapt resource value: 0x7f070030
 			public const int snap = 2131165232;
@@ -4386,8 +4368,8 @@ namespace UrbanDictionary.Xamarin.Droid
 			// aapt resource value: 0x7f070038
 			public const int start = 2131165240;
 			
-			// aapt resource value: 0x7f070089
-			public const int status_bar_latest_event_content = 2131165321;
+			// aapt resource value: 0x7f07008a
+			public const int status_bar_latest_event_content = 2131165322;
 			
 			// aapt resource value: 0x7f070060
 			public const int submenuarrow = 2131165280;
@@ -4398,11 +4380,11 @@ namespace UrbanDictionary.Xamarin.Droid
 			// aapt resource value: 0x7f070013
 			public const int tabMode = 2131165203;
 			
-			// aapt resource value: 0x7f070099
-			public const int text = 2131165337;
+			// aapt resource value: 0x7f07009a
+			public const int text = 2131165338;
 			
-			// aapt resource value: 0x7f070097
-			public const int text2 = 2131165335;
+			// aapt resource value: 0x7f070098
+			public const int text2 = 2131165336;
 			
 			// aapt resource value: 0x7f070054
 			public const int textSpacerNoButtons = 2131165268;
@@ -4410,8 +4392,8 @@ namespace UrbanDictionary.Xamarin.Droid
 			// aapt resource value: 0x7f070053
 			public const int textSpacerNoTitle = 2131165267;
 			
-			// aapt resource value: 0x7f070081
-			public const int text_input_password_toggle = 2131165313;
+			// aapt resource value: 0x7f070082
+			public const int text_input_password_toggle = 2131165314;
 			
 			// aapt resource value: 0x7f07000c
 			public const int textinput_counter = 2131165196;
@@ -4419,8 +4401,8 @@ namespace UrbanDictionary.Xamarin.Droid
 			// aapt resource value: 0x7f07000d
 			public const int textinput_error = 2131165197;
 			
-			// aapt resource value: 0x7f07008f
-			public const int time = 2131165327;
+			// aapt resource value: 0x7f070090
+			public const int time = 2131165328;
 			
 			// aapt resource value: 0x7f07004c
 			public const int title = 2131165260;
@@ -4431,8 +4413,8 @@ namespace UrbanDictionary.Xamarin.Droid
 			// aapt resource value: 0x7f070059
 			public const int title_template = 2131165273;
 			
-			// aapt resource value: 0x7f070082
-			public const int toolBar = 2131165314;
+			// aapt resource value: 0x7f070083
+			public const int toolBar = 2131165315;
 			
 			// aapt resource value: 0x7f07002b
 			public const int top = 2131165227;
@@ -4440,8 +4422,8 @@ namespace UrbanDictionary.Xamarin.Droid
 			// aapt resource value: 0x7f070058
 			public const int topPanel = 2131165272;
 			
-			// aapt resource value: 0x7f070078
-			public const int touch_outside = 2131165304;
+			// aapt resource value: 0x7f070079
+			public const int touch_outside = 2131165305;
 			
 			// aapt resource value: 0x7f07000a
 			public const int transition_current_scene = 2131165194;
@@ -4458,14 +4440,17 @@ namespace UrbanDictionary.Xamarin.Droid
 			// aapt resource value: 0x7f07000e
 			public const int view_offset_helper = 2131165198;
 			
-			// aapt resource value: 0x7f07009b
-			public const int visible = 2131165339;
+			// aapt resource value: 0x7f07009c
+			public const int visible = 2131165340;
 			
 			// aapt resource value: 0x7f070029
 			public const int withText = 2131165225;
 			
+			// aapt resource value: 0x7f070076
+			public const int wordsListView = 2131165302;
+			
 			// aapt resource value: 0x7f070075
-			public const int wordsListView = 2131165301;
+			public const int wordsListViewRefreshLayout = 2131165301;
 			
 			// aapt resource value: 0x7f070021
 			public const int wrap_content = 2131165217;
--- UrbanDictionary.Xamarin/UrbanDictionary.Xamarin/ViewModels/DayWordsViewModel.cs
+++ UrbanDictionary.Xamarin/UrbanDictionary.Xamarin/ViewModels/DayWordsViewModel.cs
@@ -15,10 +15,37 @@ namespace UrbanDictionary.Xamarin.ViewModels
         private readonly IDayWordsProvider _dayWordsProvider;
         private int pageNumber = 0;
 
+        public MvxCommand RefreshCommand { get; private set; }
+
         public DayWordsViewModel(IDayWordsProvider dayWordsProvider)
         {
             _dayWordsProvider = dayWordsProvider;
             Data = new IncrementalCollection<WordOfDay>(LoadMoreItems);
+
+            RefreshCommand = new MvxCommand(RefreshExecute);
+        }
+
+        private async void RefreshExecute()
+        {
+            try
+            {
+                IsRefreshing = true;
+                var data = await _dayWordsProvider.LoadPageWordsOfDayAsync(0);
+                Data.Clear();
+                Data.AddRange(data);
+                pageNumber = 0;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            finally
+            {
+                IsRefreshing = false;
+            }
+
+            RaisePropertyChanged(nameof(IsRefreshing));
         }
 
         private async Task<IList<WordOfDay>> LoadMoreItems()
@@ -30,9 +57,9 @@ namespace UrbanDictionary.Xamarin.ViewModels
         public override async void Start()
         {
             base.Start();
-            //var loadedData = await LoadMoreItems();
         }
 
         public IncrementalCollection<WordOfDay> Data { get; private set; }
+        public bool IsRefreshing { get; private set; }
     }
 }
